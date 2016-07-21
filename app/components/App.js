cfccae380d2e07200c285ff5181a2086666bd353
ushumpei <shumpei.uzawa@gmail.com>
2016-07-22 03:43:04 +0900

    Update mock
    少しずつUIを変更

--- app/components/App.js
+++ app/components/App.js
@@ -6,24 +6,39 @@ const App = () => (
       e.preventDefault();
       console.log('export json file');
     }}>
+    <div>
+      {'{'}
+        <div>{'key1 : hoge,'}</div>
+        <div>{'key2 : fuga,'}</div>
+        <div>
+          {'object : {'}
+            <div>{'key1 : hoge,'}</div>
+            <div>{'key2 : fuga,'}</div>
+            <div><input type="text" name="key" value="key3" />{'ok : '}<a href="#">value</a> <a href="#">object</a> <a href="#">list</a></div>
+          {'},'}
+        </div>
+        <div>
+          {'list : ['}
+            <div>{'key1,'}</div>
+            <div>{'key2'}</div>
+            <div><a href="#">value</a> <a className="inactive">object</a> <a className="inactive">list</a></div>
+          {'],'}
+        </div>
+        <div>
+          {'list2 : ['}
+            <div>{'key1,'}</div>
+            <div>{'key2,'}</div>
+            <div>
+              <input type="text" name="key" />{'ok'}
+            </div>
+          {']'}
+        </div>
+        <div><a href="#">key</a> : <a className="inactive">value</a> <a className="inactive">object</a> <a className="inactive">list</a></div>
+      {'}'}
+    </div>
       <div>
-        <span>{'{'}</span>
-          <div>
-            <input type="text" name="key" /><span>{' : '}</span><input type="text" name="value" />
-            <button type="submit" onClick={ e => {
-              e.preventDefault();
-              console.log('create json item');
-            }}>{'+'}</button>
-            <button type="submit" onClick={ e => {
-              e.preventDefault();
-              console.log('delete json item');
-            }}>{'-'}</button>
-          </div>
-        <span>{'}'}</span>
+        <a className="inactive">export</a> <a href="#">import</a> <a href="#">clear</a> <a href="#">reset</a>
       </div>
-      <button type="submit">
-        {'export'}
-      </button>
     </form>
   </div>
 )

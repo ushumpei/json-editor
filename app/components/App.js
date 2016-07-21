46aabe2291455c15c5556c04d163ecb4978ab885
ushumpei <shumpei.uzawa@gmail.com>
2016-07-22 02:41:54 +0900

    Update App component
    Appコンポーネントに基本的なformを追加

--- app/components/App.js
+++ app/components/App.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const App = () => (
   <div>
-    App Component
+    <form onSubmit={ e => {
+      e.preventDefault();
+      console.log('export json file');
+    }}>
+      <div>
+        <span>{'{'}</span>
+          <div>
+            <input type="text" name="key" /><span>{' : '}</span><input type="text" name="value" />
+            <button type="submit" onClick={ e => {
+              e.preventDefault();
+              console.log('create json item');
+            }}>{'+'}</button>
+            <button type="submit" onClick={ e => {
+              e.preventDefault();
+              console.log('delete json item');
+            }}>{'-'}</button>
+          </div>
+        <span>{'}'}</span>
+      </div>
+      <button type="submit">
+        {'export'}
+      </button>
+    </form>
   </div>
 )
 
